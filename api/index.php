02b90a2273543b8f5ccf3eecdea762a33a3fed4a
nabeelz06 <mhd.nbl.fzn@gmail.com>
2025-11-13 00:23:27 +0700

    index.php vercel.json

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
-
 <?php
 
-require __DIR__ . '/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+// --- Arahkan Vercel ke folder instalasi Laravel yang benar ---
+// Vercel menjalankan ini dari /api, jadi kita harus 'naik' satu level
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// --- Tangani Request ---
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
